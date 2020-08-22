b3b47fe1ccbc0617408ee091b75dc9d072133771
davimk <62561471+davimk@users.noreply.github.com>
2020-08-22 18:04:08 -0400

    calculate everloop images only once

--- __init__.py
+++ __init__.py
@@ -3,11 +3,11 @@ import zmq # Asynchronous messaging framework for Matrix Voice
 from matrix_io.proto.malos.v1 import driver_pb2 # MATRIX Protocol Buffer driver library
 from matrix_io.proto.malos.v1 import io_pb2 # MATRIX Protocol Buffer sensor library
 
-led_count = 18 # Number of LEDs on Matrix Voice
-matrix_ip = '127.0.0.1' # Local Matrix Voice device ip
-everloop_port = 20021 # Matrix Voice Driver Base port
+LED_COUNT = 18 # Number of LEDs on Matrix Voice
+MATRIX_IP = '127.0.0.1' # Local Matrix Voice device ip
+EVERLOOP_PORT = 20021 # Matrix Voice Driver Base port
 # Turn on all LEDs
-ringOn = {
+ON = {
         0:{"blue":50, "red":0, "green":0, "white":0},
         1:{"blue":50, "red":0, "green":0, "white":0},
         2:{"blue":50, "red":0, "green":0, "white":0},
@@ -28,9 +28,9 @@ ringOn = {
         17:{"blue":50, "red":0, "green":0, "white":0}
         }
 # Turn off all LEDs
-ringOff = {}
+OFF = {}
 # Turn on two arcs of blue LEDs while processing an utterance.
-ringThink = {
+THINK = {
             17:{"blue":50, "red":0, "green":0, "white":0},
             0:{"blue":50, "red":0, "green":0, "white":0},
             1:{"blue":50, "red":0, "green":0, "white":0},
@@ -39,7 +39,7 @@ ringThink = {
             10:{"blue":50, "red":0, "green":0, "white":0}
             }
 # Turn on two arcs of blue LEDs, 90 degrees from think arc, while speaking.
-ringSpeak = {
+SPEAK = {
             3:{"blue":50, "red":0, "green":0, "white":0},
             4:{"blue":50, "red":0, "green":0, "white":0},
             5:{"blue":50, "red":0, "green":0, "white":0},
@@ -47,6 +47,38 @@ ringSpeak = {
             14:{"blue":50, "red":0, "green":0, "white":0},
             15:{"blue":50, "red":0, "green":0, "white":0}
             }
+"""
+Control Matrix Voice LEDs
+Arguments:
+ledCount - total number of ring LEDs
+ledOn - Dictionary containing integer key of which LED to turn on (0-17).
+        Value is another dictionary containing brightness key of blue, red, green, and white
+        with integer value (0-255).
+"""
+def ring(led_count, ledOn):
+    # Create a new driver config
+    driver_config_proto = driver_pb2.DriverConfig()
+    # Create an empty Everloop image
+    image = []
+    # Set individual LED value
+    for led in range(led_count):
+        if led in ledOn:
+            ledValue = io_pb2.LedValue() # Protocol buffer custom type for Matrix Voice
+            ledValue.blue = ledOn[led]["blue"]
+            ledValue.red = ledOn[led]["red"]
+            ledValue.green = ledOn[led]["green"]
+            ledValue.white = ledOn[led]["white"]
+            image.append(ledValue)
+        else:
+            ledValue = io_pb2.LedValue()
+            ledValue.blue = 0
+            ledValue.red = 0
+            ledValue.green = 0
+            ledValue.white = 0
+            image.append(ledValue)
+    # Store the Everloop image in driver configuration
+    driver_config_proto.image.led.extend(image)
+    return driver_config_proto.SerializeToString()
 
 class LedRing(MycroftSkill):
     def __init__(self):
@@ -56,7 +88,7 @@ class LedRing(MycroftSkill):
         # Create a Pusher socket
         self.socket = self.context.socket(zmq.PUSH)
         # Connect Pusher to configuration socket
-        self.socket.connect('tcp://{0}:{1}'.format(matrix_ip, everloop_port))
+        self.socket.connect('tcp://{0}:{1}'.format(MATRIX_IP, EVERLOOP_PORT))
 
     def initialize(self):
         self.add_event('recognizer_loop:wakeword',
@@ -71,60 +103,34 @@ class LedRing(MycroftSkill):
                        self.handler_audio_output_end)
         self.add_event('mycroft.stop',
                         self.handler_mycroft_stop)
-
-
-    """
-    Control Matrix Voice LEDs
-    Arguments:
-    ledCount - total number of ring LEDs
-    ledOn - Dictionary containing integer key of which LED to turn on (0-17).
-            Value is another dictionary containing brightness key of blue, red, green, and white
-            with integer value (0-255).
-    """
-    def ring(self, ledCount, ledOn):
-        # Create a new driver config
-        driver_config_proto = driver_pb2.DriverConfig()
-        # Create an empty Everloop image
-        image = []
-        # Set individual LED value
-        for led in range(ledCount):
-            if led in ledOn:
-                ledValue = io_pb2.LedValue() # Protocol buffer custom type for Matrix Voice
-                ledValue.blue = ledOn[led]["blue"]
-                ledValue.red = ledOn[led]["red"]
-                ledValue.green = ledOn[led]["green"]
-                ledValue.white = ledOn[led]["white"]
-                image.append(ledValue)
-            else:
-                ledValue = io_pb2.LedValue()
-                ledValue.blue = 0
-                ledValue.red = 0
-                ledValue.green = 0
-                ledValue.white = 0
-                image.append(ledValue)
-        # Store the Everloop image in driver configuration
-        driver_config_proto.image.led.extend(image)
-        # Send driver configuration through ZMQ socket
-        self.socket.send(driver_config_proto.SerializeToString())
-
+        self.ring_on = ring(LED_COUNT, ON)
+        self.ring_off = ring(LED_COUNT, OFF)
+        self.ring_think = ring(LED_COUNT, THINK)
+        self.ring_speak = ring(LED_COUNT, SPEAK)
 
     def handler_wakeword(self, message):
-        self.ring(led_count, ringOn)
+        # Send driver configuration through ZMQ socket
+        self.socket.send(self.ring_on)
 
     def handler_record_end(self, message):
-        self.ring(led_count, ringOff)
+        # Send driver configuration through ZMQ socket
+        self.socket.send(self.ring_off)
 
     def handler_utterance(self, message):
-        self.ring(led_count, ringThink)
+        # Send driver configuration through ZMQ socket
+        self.socket.send(self.ring_think)
 
     def handler_audio_output_start(self, message):
-        self.ring(led_count, ringSpeak)
+        # Send driver configuration through ZMQ socket
+        self.socket.send(self.ring_speak)
 
     def handler_audio_output_end(self, message):
-        self.ring(led_count, ringOff)
+        # Send driver configuration through ZMQ socket
+        self.socket.send(self.ring_off)
 
     def handler_mycroft_stop(self, message):
-        self.ring(led_count, ringOff)
+        # Send driver configuration through ZMQ socket
+        self.socket.send(self.ring_off)
 
     def shutdown(self):
         self.socket.close()
